5ef4fddab9cb12293685de7d8be4931be13f89d1
Mirko Hernandez <mirko@fastmail.com>
2024-03-26 18:48:14 -0300

    chore: Remover uso de docs en configuración de tissue.
    Para esto es mejor usar los documentos de Doxygen (en markdown).

--- tissue.scm
+++ tissue.scm
@@ -3,15 +3,8 @@
 				    (slot-set (read-gemtext-issue filename)
 					      'web-uri
 					      (string-append "/" (string-remove-suffix ".gmi" filename))))
-				  (gemtext-files-in-directory "issues"))
-			     (map (lambda (filename)
-				    (slot-set (read-gemtext-document filename)
-					      'web-uri
-					      (string-append "/" (string-remove-suffix ".gmi" filename))))
-				  (gemtext-files-in-directory "docs")))
+				  (gemtext-files-in-directory "issues")))
  #:web-files (map (lambda (filename)
                     (file (replace-extension filename "html")
                           (gemtext-exporter filename)))
-		  (append
-		   (gemtext-files-in-directory "docs")
-                   (gemtext-files-in-directory "issues"))))
+		  (gemtext-files-in-directory "issues")))
