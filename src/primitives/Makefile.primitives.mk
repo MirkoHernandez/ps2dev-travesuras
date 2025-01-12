96ba7aa9ea60b669fb536e0f94ef0fa0e6d2ac8b
Mirko Hernandez <mirkoh@fastmail.com>
2025-01-12 00:39:11 -0300

    chore: simplificar makefiles.

---
+++ src/primitives/Makefile.primitives.mk
@@ -0,0 +1,28 @@
+EE_OBJS=main.o
+EE_CFLAGS += -Wall --std=c99
+BROWSER=qutebrowser
+
+all: compile run
+
+compile: $(EE_BIN)
+	$(EE_STRIP) --strip-all $(EE_BIN)
+
+.PHONY: run
+run:
+	pcsx2 --elf=$(mkfile_dir)/$(EE_BIN) --fullscreen --nogui
+
+.PHONY: docs
+
+docs: 
+	(cd ../../.. && doxygen Doxyfile)
+
+browse: docs
+	$(BROWSER) doxygen/html/$(DOCUMENT)
+
+
+.PHONY: clean
+clean:
+	rm -f $(EE_BIN) $(EE_OBJS)
+
+include $(PS2SDK)/samples/Makefile.eeglobal
+include $(PS2SDK)/samples/Makefile.pref
